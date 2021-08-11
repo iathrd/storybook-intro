e5f68ac829b915257c06b62b2fcbe08d663cd2f6
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-08-11 11:11:08 +0800

    storybook with redux

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,10 @@
 import Task from './Task';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { archiveTask, pinTask } from '../lib/redux';
 
 
-export default function TaskList({loading,tasks,onPinTask,onArchiveTask}) {
+export function TaskList({loading,tasks,onPinTask,onArchiveTask}) {
   const events = {
     onPinTask,
     onArchiveTask
@@ -70,3 +72,13 @@ export default function TaskList({loading,tasks,onPinTask,onArchiveTask}) {
    TaskList.defaultProps = {
     loading: false,
    };
+
+   export default connect(
+    ({ tasks }) => ({
+      tasks: tasks.filter(t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'),
+    }),
+    dispatch => ({
+      onArchiveTask: id => dispatch(archiveTask(id)),
+      onPinTask: id => dispatch(pinTask(id)),
+    })
+  )(TaskList);
